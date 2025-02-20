09ec94ff84589394633b842433eade15d4996c63
moradAbdessamad <abdessamad.morad01@gmail.com>
2025-02-20 12:14:00 +0100

    final Changes

--- hello.js
+++ hello.js
@@ -1,6 +1,12 @@
 console.log("Hello from here!")
 console.log("the change for the first commit")
 
+// Im in the process of making a change
+console.log("I'm making a change")
+console.log("I'm making a change 2")
+console.log("I'm making a change 3")
+console.log("I'm making a change 4")
+
 // Immpliment the urgent fix
 console.log("Urgent fix")
 console.log("the change for the second commit")
