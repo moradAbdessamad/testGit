22f78d709d990eff77b5aafc6834f7229d8203ac
moradAbdessamad <abdessamad.morad01@gmail.com>
2025-02-20 12:01:19 +0100

    Save the code

--- hello.js
+++ hello.js
@@ -1,2 +1,6 @@
 console.log("Hello from here!")
 console.log("the change for the first commit")
+
+// Immpliment the urgent fix
+console.log("Urgent fix")
+console.log("the change for the second commit")
